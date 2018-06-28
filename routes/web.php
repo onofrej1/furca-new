da14ae46592f9c53cf0c6e59f4e411d40e4a026c
Erik Onofrej <Erik.Onofrej@ness.com>
2018-06-28 17:28:44 +0200

    'commit'

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'article' => 'ArticleController',
+    'page' => 'PageController',
+    'menu' => 'MenuController',
+    'menuItem' => 'MenuItemController',
+    'tag' => 'TagController',
+]);
